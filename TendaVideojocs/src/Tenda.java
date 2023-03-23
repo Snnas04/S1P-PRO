5b29c82f2e49a0485f473e2e842f9c3dbd7882e9
Marc <marcsans26@gmail.com>
2023-03-23 16:33:02 +0100

    crear un progecte TendaVideojocs

---
+++ TendaVideojocs/src/Tenda.java
@@ -0,0 +1,8 @@
+public class Tenda {
+    private String ubicacio;
+    private String horari;
+    private String productes;
+    private String clients;
+    private String empleat;
+    private String proveidor;
+}
