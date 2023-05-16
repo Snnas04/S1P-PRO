f5a33ded89751811425a117834ecf4a3d799aa41
Marc <marcsans26@gmail.com>
2023-05-16 18:17:30 +0200

    eliminar figures

--- FiguresInterficie/src/Main.java
+++ FiguresInterficie/src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        new Frame("CantorDust", 6, new CantorDust());
         new Frame("sierpinski", 6, new Sierpinski());
-        new Frame("Menger", 4, new MengerCarpet());
     }
 }
